efbbc7d11ef1b25051716a35be625a89b02324ce
Ashish <itsashis4u@gmail.com>
2015-07-18 03:16:30 +0530

    Admin panel working

---
+++ client/views/create_sheet/create_sheet.js
@@ -0,0 +1,26 @@
+Template.createSheet.rendered = function() {
+
+};
+
+Template.createSheet.events({
+    "click #create": function(e, t) {
+        e.preventDefault();
+        console.log('abc');
+    }
+});
+
+Template.createSheet.helpers({
+
+});
+
+Template.createSheetJumbotron.rendered = function() {
+
+};
+
+Template.createSheetJumbotron.events({
+
+});
+
+Template.createSheetJumbotron.helpers({
+
+});
